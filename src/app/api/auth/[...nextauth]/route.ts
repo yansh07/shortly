6be9364d571c2c15162dadc6f8744cdc17acb70b
pksingh <pksingh69313@gmail.com>
2025-11-06 23:11:22 +0530

    fix: login page

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,44 +1,44 @@
-// import NextAuth, { NextAuthOptions } from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-// import GithubProvider from "next-auth/providers/github";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
-// import { PrismaClient } from "@prisma/client";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// export const authOptions: NextAuthOptions = {
-//   adapter: PrismaAdapter(prisma),
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//     GithubProvider({
-//       clientId: process.env.GITHUB_CLIENT_ID!,
-//       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-//     }),
-//   ],
-//   session:{
-//     strategy: "jwt"
-//   },
-//   callbacks: {
-//     async jwt({token, user}) {
-//       if (user) {
-//         token.id = user.id
-//       }
-//       return token;
-//     },
-//     async session({session, token}) {
-//       if (session.user) {
-//         session.user.id = token.id as string
-//       }
-//       return session
-//     }
-//   },
-//   secret: process.env.NEXTAUTH_SECRET,
-//   debug: true,
-// };
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  session:{
+    strategy: "jwt"
+  },
+  callbacks: {
+    async jwt({token, user}) {
+      if (user) {
+        token.id = user.id
+      }
+      return token;
+    },
+    // async session({session, token}) {
+    //   if (session.user) {
+    //     session.user.id = token.id as string
+    //   }
+    //   return session
+    // }
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: true,
+};
 
-// const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-// export { handler as GET, handler as POST };
+export { handler as GET, handler as POST };
